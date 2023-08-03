37e428dfa7920daaa94f11ea72fa04ae448ed2c4
Timmygee21 <saytimmy@gmail.com>
2023-08-03 12:41:02 -0700

    Create a function getStudentIdsSum that returns the sum of all the student ids

---
+++ 0x03-ES6_data_manipulation/0-main.cjs
@@ -0,0 +1,11 @@
+// 0-main.cjs
+
+// Use CommonJS require syntax
+const getListStudents = require('./0-get_list_students.js');
+
+// Use the function
+const students = getListStudents();
+console.log(students);
+
+
+
--- 0x03-ES6_data_manipulation/0-main.js
+++
@@ -1,3 +0,0 @@
-const getListStudents = require("./0-get_list_students.js");
-
-console.log(getListStudents());
