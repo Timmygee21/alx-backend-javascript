5fba0b0596a4a3fbbd6c1b91949992469e9b90f2
Timmygee21 <saytimmy@gmail.com>
2023-09-24 05:58:41 +0000

    Create a new file 4-payment.js

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,18 +1,44 @@
+const chai = require('chai');
 const sinon = require('sinon');
-const assert = require('assert');
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./4-payment');
 
-describe('sendPaymentRequestToApi', function () {
-  it('called stubbed version of calculateNumber', function () {
-    const spy = sinon.spy(console, 'log');
-    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
-    sendPaymentRequestToApi(100, 20);
+const expect = chai.expect;
 
-    assert(spy.calledOnceWith('The total is: 10'));
-    assert(stub.calledOnceWith('SUM', 100, 20));
+describe('4-payment.test sendPaymentRequestToApi', () => {
+  let apiStub = null;
 
-    spy.restore();
-    stub.restore();
+  beforeEach(() => {
+    apiStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+  });
+
+  it('should call calculateNumber with the correct parameters', () => {
+    const totalAmount = 100;
+    const totalShipping = 10;
+
+    sendPaymentRequestToApi(totalAmount, totalShipping);
+    expect(apiStub.calledWith('SUM', totalAmount, totalShipping)).to.be.true;
+  });
+
+  it('should call calculateNumber once', () => {
+    const totalAmount = 100;
+    const totalShipping = 10;
+
+    sendPaymentRequestToApi(totalAmount, totalShipping);
+    expect(apiStub.calledOnce).to.be.true;
+  });
+
+  it('should call console.log with correct parameters', () => {
+    const totalAmount = 100;
+    const totalShipping = 10;
+    sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(totalAmount, totalShipping);
+    expect(console.log.calledWith('The total is: 10')).to.be.true;
+    console.log.restore();
+  });
+
+  afterEach(() => {
+    apiStub.restore();
   });
 });
